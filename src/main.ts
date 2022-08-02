c92ed1450bca35dd5b10e0d31a11d4d65c3607ac
Damian Adams <damian.adams@rewardstyle.com>
2022-08-02 11:20:07 -0500

    Update prettier config and add Vuetify 1.x

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
 
-Vue.config.productionTip = false;
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
